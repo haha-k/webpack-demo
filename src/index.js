bc67d36ba09e07f960434ad75709e727cfa3be34
unknown <afyapu@163.com>
2019-07-20 18:39:00 +0800

    fourth commit:css loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
+import './style.css'
 
 function component(){
     var element = document.createElement('div');
 
     element.innerHTML = _.join(['haha','kk'],' ');
+    element.classList.add('hello');
 
     return element;
 }
