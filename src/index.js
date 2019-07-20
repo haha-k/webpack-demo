390908c7f81951ddf32825be48602f069688e409
unknown <afyapu@163.com>
2019-07-20 22:31:40 +0800

    fifth commit:add sass loader+sourceMap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import _ from 'lodash';
 import './style.css'
+import './style.scss';
 
 function component(){
     var element = document.createElement('div');
 
     element.innerHTML = _.join(['haha','kk'],' ');
     element.classList.add('hello');
+    element.classList.add('box');
+
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,27 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname,'dist')
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
+                test: /\.(sc|c|sa)ss$/,
+                use: [{
+                        loader: "style-loader" //将js字符串生成为style节点
+                    },
+                    {
+                        loader: "css-loader", //将css转化为CommonJs模块
+                        options: {
+                            sourceMap:true
+                        }
+                    },
+                    {
+                        loader: "sass-loader", //将sass编译成css
+                        options: {
+                            sourceMap:true
+                        }
+                    }
                 ]
             }
         ]
