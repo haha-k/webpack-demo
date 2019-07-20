412bf834f06c1fd7f86cdf4f8bac894e26a96a52
unknown <afyapu@163.com>
2019-07-20 17:10:47 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function component(){
+    var element = document.createElement('div')
+
+    element.innerHTML = _.join(['haha','kk'],' ')
+
+    return element;
+}
+
+document.body.appendChild(component());
