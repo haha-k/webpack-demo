79baeaa38c19db6888600d30d9b946622958c3a2
unknown <afyapu@163.com>
2019-07-22 12:53:44 +0800

    sixteenth commit:add hmr+webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {
     CleanWebpackPlugin
 } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
 
 const extractSass = new MiniCssExtractPlugin({
     filename: devMode ? '[name].css' : '[name].[hash].css',
@@ -17,7 +19,35 @@ const extractSass = new MiniCssExtractPlugin({
 
 module.exports = {
     entry: './src/index.js',
-    devtool: 'inline-source-map',
+    devtool: 'inline-source-map', //启用js sourceMap
+    devServer: {
+        clientLogLevel: 'none',
+        contentBase: path.join(__dirname, "public"), //告诉服务器从哪里提供内容,默认当前工作目录
+        compress: true, //一切服务都启用gzip压缩
+        hot: true, //启用webpack的模块热替换特性
+        host: '0.0.0.0', //指定一个host,默认为localhost
+        open: true, //是否打开浏览器
+        overlay: { // 出现错误或者警告时，是否全屏显示错误
+            warnings: true,
+            errors: true,
+        },
+        port:8088,//端口
+        publicPath:'/',//此路径下的打包文件可在浏览器中访问
+        proxy:{ //代理
+            "/api":{
+                target:"http://localhost:8000",
+                pathRewrite:{
+                    "^api":"",
+                }
+            }//访问 /api/users 代理到 http://localhost:8000/users
+        },
+        quiet: false, //启用 quiet 后，除了初始启动信息之外的任何内容都不会被打印到控制台。这也意味着来自 webpack 的错误或警告在控制台不可见。
+        watchOptions:{
+            poll:true,//开启polling,或者指定毫秒为单位进行轮询，1000
+            ignored:/node_modules/,//忽略文件夹
+            aggregateTimeout:300 //当第一个文件更改，会在重新构建前增加延迟。这个选项允许 webpack 将这段时间内进行的任何其他更改都聚合到一次重新构建里。以毫秒为单位：
+        }
+    },
     output: {
         filename: 'main.[hash].js',
         path: path.resolve(__dirname, 'dist')
@@ -28,7 +58,7 @@ module.exports = {
                 use: [
                     // 'file-loader',//加载图片资源
                     {
-                        loader: 'url-loader',//替换file-loader,能设置一个阈值压缩base64
+                        loader: 'url-loader', //替换file-loader,能设置一个阈值压缩base64
                         options: {
                             limit: 8192
                         }
@@ -59,8 +89,8 @@ module.exports = {
                 ]
             },
             {
-                test:/\.(woff|woff2|eot|ttf|otf)$/,
-                use:[
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
                     'url-loader'
                 ]
             },
@@ -117,6 +147,8 @@ module.exports = {
             //     removeComments:true,
             // },
         }), //生成html的插件
+        new webpack.NamedModulesPlugin(),//更容易查看（patch）的依赖
+        new webpack.HotModuleReplacementPlugin() //替换插件
     ],
     optimization: {
         minimizer: [
